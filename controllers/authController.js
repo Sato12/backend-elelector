0aabf6cc8b028f793e555959ff01cbc7ef3aa704
Sato12 <satugo0866@hotmail.com>
2021-11-21 18:49:39 -0500

    ajustes en la autoriazación

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,3 +54,15 @@ exports.autenticarUsuario = async (req, res) => {
     console.log(error);
   }
 };
+
+// Obtener usuario autenticado
+
+exports.usuarioAutenticado = async (req, res) => {
+  try {
+    const usuario = await Usuario.findById(req.usuario).select("-password");
+    res.json({ usuario });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error al autenticar usuario" });
+  }
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,21 @@
 const jwt = require("jsonwebtoken");
 
+module.exports = function (req, res, next) {
+  // Leer token del header
+  const token = req.header("x-auth-token");
 
-module.exports = function(req,res,next){
+  // Revisar si no hay token
+  if (!token) {
+    return res.status(401).json({ msg: "Sin token no hay fiesta papu" });
+  }
 
-    // Leer token del header 
-    const token = req.header("x-auth-token");
-    
+  // Validar el token
 
-    // Revisar si no hay token
-    if(!token){
-        return res.status(401).json({msg: "Sin token no hay fiesta papu"});
-    }
-
-
-    // Validar el token
-
-    try {
-        
-        const cifrado = jwt.verify(token, process.env.SECRETA);
-        req.usuario = cifrado.usuario;
-        next();
-
-    } catch (error) {
-        res.status(401).json({msg:"Token no valido"});
-    }
-}
+  try {
+    const cifrado = jwt.verify(token, process.env.SECRETA);
+    req.usuario = cifrado.usuario;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token no valido" });
+  }
+};
