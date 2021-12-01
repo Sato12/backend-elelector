1a4976429d8e049ae239309d25d47764e3e87f12
Santiago <segonzalezu@unal.edu.co>
2021-12-01 11:55:46 -0500

    2

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ conectarDB();
 
 //Habilitar CORS
 app.use(cors());
+//
+
 // Habilitar expressJSON
 app.use(express.json({ extended: true, limit: "4000kb" }));
 
 // puerto de la app
 const port = process.env.port || 8080;
+
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 
 // Importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
