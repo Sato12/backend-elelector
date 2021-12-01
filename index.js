f4fcaa2bfb6ceeab352519efa9944fcd2ecc61f1
Santiago <segonzalezu@unal.edu.co>
2021-12-01 11:15:00 -0500

    1

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.use("/api/contacto", require("./routes/contacto"));
 
 // arrancar la app
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Servidor funcionando en el puerto ${PORT}`);
+  console.log(`Servidor funcionando en el puerto ${port}`);
 });
