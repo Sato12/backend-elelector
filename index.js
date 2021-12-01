ad5d2a86bddfe956898acdd21bad4cf9f84adc21
Santiago <segonzalezu@unal.edu.co>
2021-12-01 12:53:35 -0500

    5

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ conectarDB();
 app.use(cors());
 //
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://elector-servidor.herokuapp.com/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
